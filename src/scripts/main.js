ba4a1080b1b9459e8a75191c185f076898a2f788
Ashlyn Still <ashlyn.still@gmail.com>
2015-08-31 19:06:47 -0400

    add local changes to switch to new branch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,50 @@
 $input = $('#data-input');
 
-$('#load').click(function(){
+
+var TableFunctions = {
+
+	parseData: function(input) {
+		var csv_array = input.split("\n");
+		var header = csv_array.shift();
+		header = header.split("\t");
+
+		var data = [];
+
+		_.each(csv_array, function(row){
+			row = row.split("\t");
+			var d = {};
+			_.each(row, function(r,i){
+				var this_header = header[i];
+				d[this_header] = r;
+			});
+			data.push(d);
+		});
+
+		return data
+
+	},
+
+	checkData: function(data) {
+		
+	},
+
+}
+
+
+
+
+
+
+
+
+
+
+$('#next').click(function(){
 	var results = parseData($input.val());
+
+	console.log(results);
 });
 
 $('#clear').click(function(){
 	$input.val('');
 });
-
-
-
-
-var checkData = function(data) {
-	
-};
-
-
-
-var parseData = function(input) {
-	var csv_array = input.split("\n");
-	var header = csv_array.shift();
-	header = header.split("\t");
-
-	var data = [];
-
-	_.each(csv_array, function(row){
-		row = row.split("\t");
-		var d = {};
-		_.each(row, function(r,i){
-			var this_header = header[i];
-			d[this_header] = r;
-		});
-		data.push(d);
-	});
-
-	return data
-
-};
