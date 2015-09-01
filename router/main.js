06918fd25748780ba8e70bec84b58fc9bd4bd4d2
Ashlyn Still <ashlyn.still@gmail.com>
2015-09-01 18:41:57 -0400

    swapped in node express server to replace static html buildout

---
+++ router/main.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+	
+	app.get('/',function(req,res){
+		res.render('index.html')
+	});
+
+	app.get('/table',function(req,res){
+		res.render('table.html');
+	});
+
+}
