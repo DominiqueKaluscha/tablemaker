97f8b8c516c93d1e06024efa059d0480d9b0f242
Ashlyn Still <ashlyn.still@gmail.com>
2015-09-28 12:05:46 -0400

    print user edited data to json

--- index.js
+++ index.js
@@ -2,23 +2,56 @@ var express = require( "express" ),
     nunjucks = require( "nunjucks" ),
     path = require( "path" ),
     request = require( "request" ),
+    fs = require("fs"),
     app = express(),
+    bodyParser = require('body-parser'),
     nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname, '/src' )));
 
-app.use(express.static('src'));
-// app.use('/tables', express.static('tables'));
 
-nunjucksEnv.express( app );
+var router = express.Router(); 
+app.use(express.static('src'));
+// app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use('/tables', router);
+
+// nunjucksEnv.express( app );
+
+
+
+// // middleware to use for all requests
+// router.use(function(req, res, next) {
+//     // do logging
+//     console.log('Something is happening.');
+//     next(); // make sure we go to the next routes and don't stop here
+// });
+
 
 app.get('/', function(req, res) {  
     res.render( "index.html" );
 });
 
-app.get('/tables', function (req, res) {
+// app.get('/tables',function (req,res){
+// 	res.writeHead(200, {'Content-Type': 'application/json'});
+// })
+
+
+app.post('/', function (req, res, next) {
     // input value from search
- 	var val = req.query.search;
+ 	var val = req.param("data");
+ 	res.json(val);
  	console.log(val);
+ 	var outputFile = 'tables/data/'+val.slug+'.json';
+ 	fs.writeFile(outputFile, JSON.stringify(val, null, 4), function(err) {
+	    if(err) {
+	      console.log(err);
+	    } else {
+	      console.log("JSON saved to path: " + outputFile);
+	    }
+	}); 
 });
+
+
 
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,8 @@ var li = document.getElementsByClassName('header-li'),
 var TableFunctions = {
 
 	inputData: function(id) {
+		$('#nav-row').show();
+		$('#embed-row').hide();
 		var	input = document.getElementById('data-input').value;	
 		var csv_array = input.split("\n");
 		var header = csv_array.shift();
@@ -50,10 +52,19 @@ var TableFunctions = {
 	},
 
 	chooseData: function(id) {
+		$('#nav-row').hide();
+		$('#embed-row').show();
 
-		var selected = _.filter(li, function(l) { return l.classList.contains('selected') }).map(function(s){ return s.innerHTML }).sort();
+		var selected = _.filter(li, function(l) { return l.classList.contains('selected') }).map(function(s){ return s.innerHTML });
 
-		var filteredData = _.map( data, function(d) { return _.pick(d, selected) } );
+
+		var filteredData = _.map( data, function(d) {
+			picked = _.pick(d, selected);
+
+			return picked;
+			// console.log(picked);
+			// return picked.reverse();
+		});
 
 		var main = document.getElementById('input'+id).children[0].children[0].children['main'];
 
@@ -69,7 +80,6 @@ var TableFunctions = {
 
 		keys.forEach(function(s){
 			table.firstChild.firstChild.innerHTML += '<th class="header-name" id="header_'+s+'"><div class="header-view"><span>'+s+'</span> <i id="edit_'+s+'" class="fa fa-pencil"></i></div><div class="header-input"><div class="row collapse postfix-radius"><div class="small-11 columns"><input type="text"/></div><div class="small-1 columns"><span class="postfix save" id="save_'+s+'"><i class="fa fa-floppy-o"></i></span></div></div></div></th>';
-			table.firstChild.children[1].innerHTML += '<th id="type_'+s+'"><select><option value="text">Text</option><option value="number">Number</option></select></th>';
 
 		});
 
@@ -82,7 +92,8 @@ var TableFunctions = {
 			$header_val = $th.children('.header-view').children('span').text();
 			$th.children('.header-input').show();
 			$th.children('.header-input').children('.postfix-radius').children('.small-11').children('input').val($header_val);
-		})
+		});
+
 		$('th.header-name .postfix.save').click(function(){
 			$id = $(this).attr('id').replace('save_','');
 			$th = $('#header_'+$id);
@@ -107,13 +118,25 @@ var TableFunctions = {
 
 	},
 
-	// callAjax: function(){
-	// 	// $('button').click(function () {
-	//     $.post('/', {data: filtered}, function (data) {
-	//         console.log(data);
-	//     });
-	//     // }, 'json');
-	// }
+	callAjax: function(id){
+
+		var title = $('#table-title').val(),
+			chatter = $('#table-chatter').val(),
+			slug = title.replace(/(\W+)/g,'-');
+
+
+		var returnData = {
+			"title": title,
+			"slug": slug,
+			"chatter": chatter,
+			"data": filtered
+		}
+
+
+	    $.post('/', {data: returnData}, function (data) {
+	        console.log(data);
+	    });
+	}
 
 }
 
@@ -125,24 +148,32 @@ var TableFunctions = {
 var currentID = 0,
 	func = _.keys(TableFunctions),
 	nextBtn = document.getElementById('next'),
-	backBtn = document.getElementById('back');
+	backBtn = document.getElementById('back'),
+	embedBtn = document.getElementById('embed');
   
 nextBtn.addEventListener('click', function(){
-
 	document.getElementById('input'+currentID).style.display='none';
 	TableFunctions[func[currentID]](currentID+1);
 
-	if (currentID<(func.length)){
+	// if (currentID<(func.length)){
 		currentID++;
 		document.getElementById('input'+currentID).style.display='block';
-	}
+	// }
 
+});
+
+embedBtn.addEventListener('click', function(){
+	TableFunctions[func[currentID]](currentID+1);
+	alert('clicked!',currentID+1);
 });
 
 backBtn.addEventListener('click', function(){
 	currentID--;
 	TableFunctions[func[currentID]]();
 });
+
+
+
 
 
 // $.get( '/tables', params, function(data){
